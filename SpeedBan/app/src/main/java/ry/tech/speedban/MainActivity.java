a13801445af9c5cafd6b4785a58ad7b8fede3eb7
jxpix <wouldress@gmail.com>
2024-11-23 21:53:47 +0300

    df

--- SpeedBan/app/src/main/java/ry/tech/speedban/MainActivity.java
+++ SpeedBan/app/src/main/java/ry/tech/speedban/MainActivity.java
@@ -1,24 +1,38 @@
 package ry.tech.speedban;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        Button lessonsButton = findViewById(R.id.lessonsButton);
+        Button quizzesButton = findViewById(R.id.quizzesButton);
+        Button profileButton = findViewById(R.id.profileButton);
+
+        lessonsButton.setOnClickListener(view -> switchFragment(new LessonsFragment()));
+        quizzesButton.setOnClickListener(view -> switchFragment(new QuizzesFragment()));
+        profileButton.setOnClickListener(view -> switchFragment(new ProfileFragment()));
+
+        if (savedInstanceState == null) {
+            switchFragment(new LessonsFragment());
+        }
+    }
+    private void switchFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragmentContainer, fragment)
+                .commit();
     }
 }
